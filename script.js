5688788ee24d859482a4d67614c8e4c13643c25f
gchkadua3 <120250936+gchkadua3@users.noreply.github.com>
2023-05-11 14:51:48 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function gamotvla() {
+
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+
+    let addition = num1 + num2;
+    let division = num1 / num2;
+    let multiplication = num1 * num2;
+    let subtraction = num1 - num2;
+
+    console.log(addition);
+    console.log(division);
+    console.log(multiplication);
+    console.log(subtraction);
+    
+}
